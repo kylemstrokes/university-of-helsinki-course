2632af36c6fcb6009d5be895265fb628f3cfc473
kylemstrokes <kylemstrokes@gmail.com>
2025-12-29 16:37:00 -0500

    vite configs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+    preview: {
+    host: true,
+    port: Number(process.env.PORT) || 4173,
+    strictPort: true,
+    allowedHosts: [
+      "university-of-helsinki-course-1.onrender.com",
+      // or ".onrender.com"
+    ],
+  },
+  server: {
+    host: true, // listen on 0.0.0.0
+    port: Number(process.env.PORT) || 5173,
+    strictPort: true,
+    allowedHosts: [
+      "university-of-helsinki-course-1.onrender.com",
+      // or use a wildcard-style entry:
+      // ".onrender.com",
+    ],
+  },
 })
+
+
